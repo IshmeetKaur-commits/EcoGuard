8ce51490f16ba9331d332e7b2949d7506a728369
Ishmeet Kaur <ishmeetkaur9511@gmail.com>
2026-06-09 23:42:05 +0530

    feat: add energy calculation engine

--- modules/carbon/carbonCalculator.js
+++ modules/carbon/carbonCalculator.js
@@ -1,29 +1,41 @@
 const WEBSITE_TYPES = {
 
     video: [
-        "youtube.com",
-        "netflix.com",
-        "primevideo.com"
-    ],
+    "youtube.com",
+    "www.youtube.com",
+    "netflix.com",
+    "www.netflix.com",
+    "primevideo.com",
+    "www.primevideo.com"
+],
 
-    social: [
-        "facebook.com",
-        "instagram.com",
-        "x.com"
-    ],
+social: [
+    "facebook.com",
+    "www.facebook.com",
+    "instagram.com",
+    "www.instagram.com",
+    "x.com",
+    "www.x.com"
+],
 
-    search: [
-        "google.com",
-        "bing.com"
-    ],
+search: [
+    "google.com",
+    "www.google.com",
+    "bing.com",
+    "www.bing.com"
+],
 
-    news: [
-        "bbc.com",
-        "cnn.com"
-    ]
+news: [
+    "bbc.com",
+    "www.bbc.com",
+    "cnn.com",
+    "www.cnn.com"
+]
 };
 
 function getWebsiteType(domain) {
+
+    domain = domain.toLowerCase();
 
     if (WEBSITE_TYPES.video.includes(domain))
         return "video";
@@ -40,6 +52,19 @@ function getWebsiteType(domain) {
     return "default";
 }
 
+const ENERGY_RATES = {
+
+    video: 0.08,
+
+    social: 0.05,
+
+    search: 0.02,
+
+    news: 0.03,
+
+    default: 0.04
+};
+
 export function calculateCarbon(browsingData) {
 
     const {
@@ -48,12 +73,16 @@ export function calculateCarbon(browsingData) {
     } = browsingData;
 
     const websiteType = getWebsiteType(domain);
+    
+    const minutes = visitTime / 60;
+    const energyUsed =
+    minutes *
+    ENERGY_RATES[websiteType];
 
     return {
         domain,
         visitTime,
-        websiteType,
         carbonEmission: 0,
-        energyUsed: 0
+        energyUsed
     };
 }
